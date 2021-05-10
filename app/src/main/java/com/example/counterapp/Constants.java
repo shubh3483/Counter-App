076abceb52026b88894e36d612ab91203ceb8f5d
Shubh Agrawal <shubh.coding@gmail.com>
2021-05-10 17:10:13 +0530

    Added data loss prevention and storing methods

---
+++ app/src/main/java/com/example/counterapp/Constants.java
@@ -0,0 +1,5 @@
+package com.example.counterapp;
+
+public class Constants {
+    public static final String QTY = "qty";
+}
--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.counterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.View;
 
 import com.example.counterapp.databinding.ActivityMainBinding;
@@ -23,8 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
         b = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(b.getRoot());
-
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
         setUpEventHandlers();
+        if(savedInstanceState != null){
+            qty = savedInstanceState.getInt(Constants.QTY);
+            b.resultTextView.setText(String.valueOf(qty));
+        }else{
+            qty = preferences.getInt(Constants.QTY, 0);
+            b.resultTextView.setText(String.valueOf(qty));
+        }
     }
 
     private void setUpEventHandlers() {
@@ -41,6 +51,21 @@ public class MainActivity extends AppCompatActivity {
                 decQty();
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(Constants.QTY, qty);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences preferences = getPreferences(MODE_PRIVATE);
+        preferences.edit()
+                .putInt(Constants.QTY, qty)
+                .apply();
     }
 
     /**
